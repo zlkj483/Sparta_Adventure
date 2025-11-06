62a4123e2175198767ca8cea6e65cd35a06e925f
Lim <mlimsg08@gmail.com>
2025-11-06 20:59:42 +0900

    feat : Add Script, Add Scirpt Editor

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    private static CharacterManager _instance;
+    public static CharacterManager Instance
+    {
+        get
+        {
+            if( _instance == null )
+            {
+                _instance = new CharacterManager();
+            }
+            return _instance;
+        }
+    }
+    public Player _player;
+    public Player Player
+    {
+        get { return _player; }
+        set { _player = value; }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+
+        }
+    }
+}
